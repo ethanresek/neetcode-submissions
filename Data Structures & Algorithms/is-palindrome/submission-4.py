e489696da8ae19487b90d90e3435dab11ccf5e00
Ethan Resek <resekethan@gmail.com>
2026-09-03 15:42:37 -0400

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+
+        cleaned = s.lower()
+
+        front = 0
+        back = len(cleaned) - 1
+
+        while front < back:
+            if not cleaned[front].isalnum():
+                front += 1
+            elif not cleaned[back].isalnum():
+                back -= 1
+            else:            
+                if cleaned[front] != cleaned[back]:
+                    return False
+                front += 1
+                back -= 1
+        
+        return True
